d59fae586d1d80d89a6796f055db5ec5a61be4e0
dykov <dykovoleksii@gmail.com>
2023-03-01 23:09:58 +0200

    Add 3-0-3-account-dao solution

--- 3-0-jpa-and-hibernate/3-0-3-account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
+++ 3-0-jpa-and-hibernate/3-0-3-account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -1,9 +1,11 @@
 package com.bobocode.dao;
 
+import com.bobocode.exception.AccountDaoException;
 import com.bobocode.model.Account;
-import com.bobocode.util.ExerciseNotCompletedException;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class AccountDaoImpl implements AccountDao {
@@ -15,32 +17,105 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.persist(account);
+            entityManager.getTransaction().commit();
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot save", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Account findById(Long id) {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            final Account account = entityManager.find(Account.class, id);
+            entityManager.getTransaction().commit();
+            return account;
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot find by id", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Account findByEmail(String email) {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            final TypedQuery<Account> namedQuery = entityManager.createQuery(
+                    "SELECT a FROM Account a WHERE a.email = :email",
+                    Account.class
+            );
+            namedQuery.setParameter("email", email);
+            final Account account = namedQuery.getSingleResult();
+            entityManager.getTransaction().commit();
+            return account;
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot find by email", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public List<Account> findAll() {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            final TypedQuery<Account> query = entityManager.createQuery(
+                    "SELECT a FROM Account a",
+                    Account.class
+            );
+            final List<Account> accounts = query.getResultList();
+            entityManager.getTransaction().commit();
+            return accounts;
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot find all", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void update(Account account) {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.merge(account);
+            entityManager.getTransaction().commit();
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot update", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void remove(Account account) {
-        throw new ExerciseNotCompletedException(); // todo
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            final Account merge = entityManager.merge(account);
+            entityManager.remove(merge);
+            entityManager.getTransaction().commit();
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Cannot remove", e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
 
