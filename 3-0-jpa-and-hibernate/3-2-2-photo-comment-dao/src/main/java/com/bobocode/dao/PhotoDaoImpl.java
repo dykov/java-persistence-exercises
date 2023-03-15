912a2afd0e7940f48f3755bf43186df68a984b5d
dykov <dykovoleksii@gmail.com>
2023-03-15 23:03:08 +0200

    Add 3-2-2-photo-comment-dao solution

--- 3-0-jpa-and-hibernate/3-2-2-photo-comment-dao/src/main/java/com/bobocode/dao/PhotoDaoImpl.java
+++ 3-0-jpa-and-hibernate/3-2-2-photo-comment-dao/src/main/java/com/bobocode/dao/PhotoDaoImpl.java
@@ -1,16 +1,20 @@
 package com.bobocode.dao;
 
 import com.bobocode.model.Photo;
+import com.bobocode.model.PhotoComment;
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Please note that you should not use auto-commit mode for your implementation.
  */
 public class PhotoDaoImpl implements PhotoDao {
-    private EntityManagerFactory entityManagerFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
     public PhotoDaoImpl(EntityManagerFactory entityManagerFactory) {
         this.entityManagerFactory = entityManagerFactory;
@@ -18,26 +22,74 @@ public class PhotoDaoImpl implements PhotoDao {
 
     @Override
     public void save(Photo photo) {
-        throw new ExerciseNotCompletedException(); // todo
+        performInTransaction(
+                entityManager -> entityManager.persist(photo)
+        );
     }
 
     @Override
     public Photo findById(long id) {
-        throw new ExerciseNotCompletedException(); // todo
+        return performAndReturnInTransaction(
+                entityManager -> entityManager.find(Photo.class, id)
+        );
     }
 
     @Override
     public List<Photo> findAll() {
-        throw new ExerciseNotCompletedException(); // todo
+        return performAndReturnInTransaction(
+                entityManager -> entityManager.createQuery("select p from Photo p").getResultList()
+        );
     }
 
     @Override
     public void remove(Photo photo) {
-        throw new ExerciseNotCompletedException(); // todo
+        performInTransaction(
+                entityManager -> {
+                    final Photo merged = entityManager.merge(photo);
+                    entityManager.remove(merged);
+                }
+        );
     }
 
     @Override
     public void addComment(long photoId, String comment) {
-        throw new ExerciseNotCompletedException(); // todo
+        performInTransaction(
+                entityManager -> {
+                    final Photo photo = entityManager.getReference(Photo.class, photoId);
+                    PhotoComment photoComment = new PhotoComment(comment, photo);
+                    entityManager.persist(photoComment);
+                }
+        );
     }
+
+
+    private void performInTransaction(final Consumer<EntityManager> entityManagerConsumer) {
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManagerConsumer.accept(entityManager);
+            entityManager.getTransaction().commit();
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private <T> T performAndReturnInTransaction(final Function<EntityManager, T> entityManagerTFunction) {
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            final T result = entityManagerTFunction.apply(entityManager);
+            entityManager.getTransaction().commit();
+            return result;
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        } finally {
+            entityManager.close();
+        }
+    }
+
 }
--- 3-0-jpa-and-hibernate/3-2-2-photo-comment-dao/src/main/java/com/bobocode/model/Photo.java
+++ 3-0-jpa-and-hibernate/3-2-2-photo-comment-dao/src/main/java/com/bobocode/model/Photo.java
@@ -1,21 +1,25 @@
 package com.bobocode.model;
 
 import com.bobocode.util.ExerciseNotCompletedException;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * todo:
  * - make a setter for field {@link Photo#comments} {@code private}
  * - implement equals() and hashCode() based on identifier field
- *
+ * <p>
  * - configure JPA entity
  * - specify table name: "photo"
  * - configure auto generated identifier
  * - configure not nullable and unique column: url
- *
+ * <p>
  * - initialize field comments
  * - map relation between Photo and PhotoComment on the child side
  * - implement helper methods {@link Photo#addComment(PhotoComment)} and {@link Photo#removeComment(PhotoComment)}
@@ -24,17 +28,38 @@ import java.util.List;
  */
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity
+@Table(name = "photo")
 public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String url;
+
     private String description;
-    private List<PhotoComment> comments;
+
+    @OneToMany(
+            mappedBy = "photo",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<PhotoComment> comments = new ArrayList<>();
+
+    private void setComments(final List<PhotoComment> comments) {
+        this.comments = comments;
+    }
 
     public void addComment(PhotoComment comment) {
-        throw new ExerciseNotCompletedException();
+        this.comments.add(comment);
+        comment.setPhoto(this);
     }
 
     public void removeComment(PhotoComment comment) {
-        throw new ExerciseNotCompletedException();
+        this.comments.remove(comment);
+        comment.setPhoto(null);
     }
 }
